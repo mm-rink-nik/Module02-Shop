bf8b28e8f349bc35c213cef32faf17f983ea419a
mm.rink.nik <mm.rink.nik@gmail.com>
2022-04-27 00:03:44 +0300

    add mobal

--- src/index.js
+++ src/index.js
@@ -21,6 +21,21 @@ var swiper = new Swiper(".offerSwiper", {
       prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1000: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+    }
   });
 
   var swiper = new Swiper(".trendsSwiper", {
@@ -38,4 +53,19 @@ var swiper = new Swiper(".offerSwiper", {
       prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      1000: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+    }
   });
